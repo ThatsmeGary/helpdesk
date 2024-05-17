f98649a994356d11c93ddb3361d568b458f25941
ThatsmeGary <gabriel.souza.dasilva@outlook.com>
2024-05-17 00:28:12 -0300

    Enums implemented

---
+++ src/test/java/com/gary/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
